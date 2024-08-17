5b069ce9d08210bd9c14d8906ecdceb1d11b1b02
ALucifr <koirala.aagyaman111@gmail.com>
2024-08-17 10:21:53 +0545

    Update Code

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,13 +3,15 @@ import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
+    let imageURL =
+      "https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg";
     return (
       <>
         <div className="container my-3">
           <h2>NewsDog - Top Headlines</h2>
           <div className="row">
             <div className="col-md-3">
-              <NewsItem />
+              <NewsItem imageURL={imageURL} />
             </div>
             <div className="col-md-3">
               <NewsItem />
